169a908d4718a4c636e79d08f7bfeaec02f19094
Najatul Muslim Dinatra <najatulmuslimdinatra@gmail.com>
2024-07-30 23:49:38 +0700

    install oppenzepelin contracts

--- src/BottleToken.sol
+++ src/BottleToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract BottleToken is ERC20, Ownable {
     constructor() ERC20("BottleToken", "BTL") {
