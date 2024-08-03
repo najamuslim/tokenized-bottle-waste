940713792f75fb3f6f85d1564b52f6461899a370
Najatul Muslim Dinatra <najatulmuslimdinatra@gmail.com>
2024-08-03 21:25:31 +0700

    updated contract using openzeppelin generator

--- src/BottleToken.sol
+++ src/BottleToken.sol
@@ -6,27 +6,50 @@ import "lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract BottleToken is ERC20, Ownable {
-    constructor() ERC20("BottleToken", "BTL") {
-        _mint(msg.sender, 1000 * 10 ** decimals());
-    }
+contract XottleToken is ERC20, Ownable {
+    constructor(address initialOwner)
+        ERC20("XOTTLE", "XOTL")
+        Ownable(initialOwner)
+    {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 }
 
-contract BottleNFT is ERC721URIStorage, Ownable {
-    uint256 public tokenCounter;
+contract XottleNFT is ERC721, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
 
-    constructor() ERC721("BottleNFT", "BNFT") {
-        tokenCounter = 0;
+    constructor(address initialOwner)
+        ERC721("XottleNFT", "XOFT")
+        Ownable(initialOwner)
+    {}
+
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
 
-    function mintNFT(address to, string memory tokenURI) public onlyOwner {
-        _safeMint(to, tokenCounter);
-        _setTokenURI(tokenCounter, tokenURI);
-        tokenCounter++;
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
+
 
