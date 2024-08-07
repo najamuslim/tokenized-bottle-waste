234518a56a7e10b60e2d92b0f9f2d9547e8406f4
rioyuda02 <rio.yudayanto244@gmail.com>
2024-08-08 01:35:42 +0700

    update fix tonight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./assets/css/globals.css";
 import "./assets/css/topcontent.css";
 
 const inter = Inter({ subsets: ["latin"] });
