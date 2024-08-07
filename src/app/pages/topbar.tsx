95965f0674408e0861e75def7d4f7e719c77a2c8
rioyuda02 <rio.yudayanto244@gmail.com>
2024-08-07 12:39:28 +0700

    update today

--- src/app/pages/topbar.tsx
+++ src/app/pages/topbar.tsx
@@ -5,6 +5,8 @@ import { ethers } from "ethers";
 import type { CustomFlowbiteTheme } from "flowbite-react";
 import { Modal, Sidebar } from "flowbite-react";
 import Image from "next/image";
+
+//Import Assets SVG Icon
 import connect_icon from "../assets/svg/connect_.svg";
 import Q1_icon from "../assets/svg/Q_1.svg";
 import Logo_brand from "../assets/svg/Xottle_Logo.svg";
@@ -12,10 +14,13 @@ import Metamask_icon from "../assets/svg/metamask.svg";
 import Coinbase_icon from "../assets/svg/CoinbaseWallet.svg";
 import ConnectWallet_icon from "../assets/svg/Wallet_Connect.svg";
 import BraveWallet_icon from "../assets/svg/Brave_Wallet.svg";
-import { motion } from "framer-motion";
 import Toggle_On from "../assets/svg/toggle-on.svg";
 import Toggle_Off from "../assets/svg/toggle-off.svg";
-import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+import Inbox_icon from "../assets/svg/Inbox.svg";
+
+import { motion } from "framer-motion";
+import { HiChartPie, HiInbox, HiShoppingBag } from "react-icons/hi";
+import { isAddress } from "ethers/src.ts/utils";
 
 //Properties
 const Ethereum = (window as any).ethereum;
@@ -75,6 +80,12 @@ const Top_Bar = () => {
     //Close Modal
     setOpenModal(false);
   };
+
+  const getAddress = () => {
+     const account = provider.getSigner();
+    account.getAddress();
+
+  }
 //CUSTOM THEME
   const customTheme: CustomFlowbiteTheme["navbar"] = {
     root: {
@@ -83,7 +94,7 @@ const Top_Bar = () => {
         on: "rounded",
         off: "",
       },
-    }
+    },
     //END CUSTOM THEME
   };
 
@@ -100,21 +111,34 @@ const Top_Bar = () => {
 
   //
   const [isOn, setIsOn] = useState(false);
+
+
+  //Slice Address Wallet Text
+ 
                 
     return (
       <>
         <nav className="Navbar">
+          {/* Inbox Messages */}
+          <div id="inbox-msg">
+            <label>3</label>
+            <span>
+              <Image src={Inbox_icon} alt="Inbox" />
+            </span>
+            Inbox
+          </div>
           <div className="h-nav">
             <div className="l-nav">
               {/* Mobile menu button */}
               <button
+                id="btn-toggle"
                 className="md:hidden w-14 h-auto"
                 onClick={toggleMobileMenu}
               >
                 <Switch isOn={isOn} onClick={() => setIsOn(!isOn)} />
               </button>
               {/* Logo */}
-              <a href="/" className="text-xl font-bold text-white">
+              <a href="/">
                 <Image
                   className="logo-brand"
                   src={Logo_brand}
@@ -122,27 +146,19 @@ const Top_Bar = () => {
                 />
               </a>
               {/* Desktop navigation links */}
-              <div className="hidden md:flex space-x-4">
-                <a href="#" className="text-gray-300 hover:text-white">
-                  Store
-                </a>
-                <a href="#" className="text-gray-300 hover:text-white">
-                  Account
-                </a>
+              <div className="Menu-nav">
+                <a href="#">Dashboard</a>
+                <a href="#">Store</a>
               </div>
-              <div className="flex md:order-2">
+              <div className="con-btn">
                 <button
                   id="btnConnect"
                   onClick={() => setOpenModal(true)}
                   type="button"
                   data-modal-target="crypto-modal"
                 >
-                  <Image
-                    className="connect-icon"
-                    src={connect_icon}
-                    alt="Connect Wallet icon"
-                  ></Image>
-                  <span>{defaultAccount ? "Connected" : "Connect Wallet"}</span>
+                  <Image src={connect_icon} alt="Connect Wallet icon"></Image>
+                  <span>{defaultAccount ? defaultAccount.slice(0, 12 - 1 , ) + '...' : "Connect Wallet"}</span>
                 </button>
               </div>
             </div>
@@ -151,7 +167,8 @@ const Top_Bar = () => {
               <div
                 className={`md:hidden ${
                   isMobileMenuOpen ? "block" : "hidden"
-                } bg-transparent`}>
+                } bg-transparent`}
+              >
                 <Sidebar aria-label="Default sidebar example">
                   <Sidebar.Items>
                     <Sidebar.ItemGroup>
@@ -164,15 +181,96 @@ const Top_Bar = () => {
                       <Sidebar.Item href="#" icon={HiShoppingBag}>
                         Store
                       </Sidebar.Item>
-                      <Sidebar.Item href="#" icon={HiArrowSmRight}>
-                        Profile
-                      </Sidebar.Item>
                     </Sidebar.ItemGroup>
                   </Sidebar.Items>
                 </Sidebar>
               </div>
             </div>
           </div>
+
+          <Modal show={openModal} onClose={() => setOpenModal(false)}>
+            <Modal.Header>
+              <div className="flex items-center justify-between p-2 md:p-6">
+                <h3 className="text-lg font-semibold text-gray-700 dark:text-white">
+                  Connect wallet
+                </h3>
+              </div>
+            </Modal.Header>
+
+            <Modal.Body>
+              <div className="p-4 md:p-5">
+                <p className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  Connect with one of our available wallet providers or create a
+                  new one.
+                </p>
+                <ul className="my-6 space-y-2">
+                  <li>
+                    <span
+                      id="metamask_w"
+                      className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+                      onClick={connectWalletHandler}
+                    >
+                      <Image src={Metamask_icon} alt="Metamask icon" />
+                      <span className="flex-1 ms-3 whitespace-nowrap">
+                        MetaMask
+                      </span>
+                      <span className="popular">Popular</span>
+                    </span>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+                    >
+                      <Image src={Coinbase_icon} alt="Coinbase icon" />
+                      <span className="flex-1 ms-3 whitespace-nowrap">
+                        Coinbase Wallet
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+                    >
+                      <Image
+                        src={ConnectWallet_icon}
+                        alt="Wallet Connect icon"
+                      />
+                      <span className="flex-1 ms-3 whitespace-nowrap">
+                        Wallet Connect
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="#"
+                      className="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white"
+                    >
+                      <Image src={BraveWallet_icon} alt="Wallet Connect icon" />
+                      <span className="flex-1 ms-3 whitespace-nowrap">
+                        Brave Wallet
+                      </span>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <div>
+                <a
+                  href="#"
+                  className="inline-flex items-center text-xs font-normal text-gray-500 hover:underline dark:text-gray-400"
+                >
+                  <Image src={Q1_icon} alt="Q-1 icon" />
+                  <span className="ml-2">
+                    Why do I need to connect with my wallet?
+                  </span>
+                </a>
+              </div>
+            </Modal.Footer>
+          </Modal>
         </nav>
       </>
     );
